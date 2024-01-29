5e2e30aebddcd5de9afed6b7d78389d50127a5d4
AdilsonManuel1 <50201412+AdilsonManuel1@users.noreply.github.com>
2024-01-29 15:09:48 +0100

    estudo do typscript para angular

---
+++ typescript para angular/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function printaObjecto(pessoa) {
+    console.log(pessoa);
+}
+printaObjecto({
+    name: "Adilson Manuel",
+    vulgo: "Zorro",
+});
+console.log("Olá Mundo TS");
---
+++ typescript para angular/src/index.ts
@@ -0,0 +1,15 @@
+type heroi ={
+    name: string,
+    vulgo:string;
+
+}
+function printaObjecto(pessoa: heroi){
+    console.log(pessoa);
+
+}
+printaObjecto({
+    name: "Adilson Manuel",
+    vulgo: "Zorro",
+});
+
+console.log("Olá Mundo TS");
